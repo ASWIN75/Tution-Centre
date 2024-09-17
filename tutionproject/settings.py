e794f95d455e2e16e08de412a9c3be97aff3b0bd
Aswin V <aswinaashi7@gmail.com>
2024-09-17 10:43:17 +0530

    Tution Centre Project

--- tutionproject/settings.py
+++ tutionproject/settings.py
@@ -144,5 +144,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'aswinaashi7@gmail.com'
-EMAIL_HOST_PASSWORD = 'rstj szyu bnhx graz'
+EMAIL_HOST_PASSWORD = 'your password'
 
